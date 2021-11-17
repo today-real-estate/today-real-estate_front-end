c562b87b03ecc80246670a3819f2cba60c822189
JeongHwan-dev <jeonghwan.dev@gmail.com>
2021-11-17 10:45:10 +0900

    Feat : 헤더, 메인 페이지 검색 컨테이너 마크업 및 스타일링

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,22 @@
 <template>
 	<div id="app">
+		<Header />
 		<RouterView />
 	</div>
 </template>
 
 <script>
+import Header from '@/components/common/Header.vue';
+
 export default {
 	name: 'App',
-	components: {},
+	components: {
+		Header,
+	},
 };
 </script>
 
 <style>
 @import './css/reset.css';
+@import './css/common.css';
 </style>

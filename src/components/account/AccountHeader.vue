af1f2371a357eb3d69429b9b6c72ab483efdd24f
JeongHwan-dev <jeonghwan.dev@gmail.com>
2021-11-18 14:02:21 +0900

    Refactor : scss 모듈화 및 코드 리팩터링

--- src/components/account/AccountHeader.vue
+++ src/components/account/AccountHeader.vue
@@ -20,37 +20,5 @@ export default {};
 </script>
 
 <style lang="scss" scoped>
-$light-dark: #3a3a3a;
-
-.account-header {
-	width: 1100px;
-	margin: 0 auto;
-
-	.account-header__title {
-		padding: 10px 0;
-		font-size: 36px;
-		font-weight: 400;
-		text-align: center;
-	}
-	.account-header__nav {
-		display: flex;
-		justify-content: space-around;
-		align-items: center;
-		height: 48px;
-		margin-bottom: 0;
-		border-bottom: 1px solid #cacaca;
-		font-size: 18px;
-		text-align: center;
-
-		.nav__link {
-			width: 200px;
-			margin: 0 auto;
-		}
-
-		a {
-			color: $light-dark;
-			text-align: center;
-		}
-	}
-}
+@import './scss/accountHeader.scss';
 </style>

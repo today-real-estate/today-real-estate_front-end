1f539f359001232a9aa4bb763f5f278861ab9a7a
JeongHwan-dev <jeonghwan.dev@gmail.com>
2021-11-19 10:32:03 +0900

    Design : 헤더 계정 메뉴 부분 디자인 수정

--- src/components/common/Header.vue
+++ src/components/common/Header.vue
@@ -14,31 +14,54 @@
 			</template>
 			<template v-else>
 				<div class="header__user">
-					<ADropdown-button style="font-size: 16px">
+					<ADropdown-button size="large">
 						{{ getNickname }}
-						<AMenu slot="overlay">
-							<AMenu-item key="1">
-								<RouterLink to="/account/edit" style="font-size: 16px">
+						<AMenu slot="overlay" style="padding: 0; border-radius: 3px">
+							<AMenu-item
+								key="1"
+								style="padding: 16px 25px; border-radius: 3px 3px 0 0"
+							>
+								<RouterLink
+									to="/account/edit"
+									style="
+										width: 200px;
+										font-size: 16px;
+										font-family: 'Spoqa Han Sans Neo', 'sans-serif';
+									"
+								>
 									내 정보
 								</RouterLink>
 							</AMenu-item>
-							<AMenu-item key="2">
-								<RouterLink to="/account/inquiry-list" style="font-size: 16px">
+							<AMenu-item key="2" style="padding: 16px 25px">
+								<RouterLink
+									to="/account/inquiry-list"
+									style="
+										width: 200px;
+										font-size: 16px;
+										font-family: 'Spoqa Han Sans Neo', 'sans-serif';
+									"
+								>
 									1:1 문의 내역
 								</RouterLink>
 							</AMenu-item>
-							<AMenu-item key="3">
-								<button
-									@click="clickLogout"
+							<AMenu-item
+								key="3"
+								style="padding: 16px 10px; border-radius: 0 0 3px 3px"
+							>
+								<AButton
+									@click="showConfirm"
 									style="
+										width: 200px;
 										border: none;
 										background-color: transparent;
 										font-size: 16px;
+										text-align: left;
 										cursor: pointer;
+										font-family: 'Spoqa Han Sans Neo', 'sans-serif';
 									"
 								>
 									로그아웃
-								</button>
+								</AButton>
 							</AMenu-item>
 						</AMenu>
 						<AIcon slot="icon" type="user" />
@@ -52,6 +75,8 @@
 <script>
 import { mapGetters } from 'vuex';
 import { clearAllCookies } from '@/utils/cookies';
+import store from '@/store/index';
+import router from '@/routes/index';
 
 export default {
 	computed: {
@@ -64,6 +89,26 @@ export default {
 			this.$router.history.current.fullPath === '/'
 				? this.$router.go()
 				: this.$router.push('/');
+		},
+		showConfirm() {
+			this.$confirm({
+				title: () => (
+					<h1 style="font-size: 18px; font-weight: 200; font-family: 'Spoqa Han Sans Neo', 'sans-serif'; color: #000;">
+						로그아웃 하시겠습니까?
+					</h1>
+				),
+				okText: '확인',
+				cancelText: '취소',
+				onOk() {
+					store.dispatch('userStore/LOGOUT');
+					clearAllCookies();
+					router.history.current.fullPath === '/'
+						? router.go()
+						: router.push('/');
+				},
+				onCancel() {},
+				class: 'test',
+			});
 		},
 	},
 };

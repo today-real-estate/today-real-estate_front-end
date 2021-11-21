4828b839e2858128725862203dd0639fc09f75d5
JeongHwan-dev <jeonghwan.dev@gmail.com>
2021-11-22 00:11:13 +0900

    Feat : 아파트 매물 상세 정보 컴포넌트 구현

--- src/components/search/MapMain.vue
+++ src/components/search/MapMain.vue
@@ -2,51 +2,84 @@
 	<div class="map__main">
 		<div class="main__side-bar">
 			<div class="side-bar__header">
-				<span>검색된 아파트 {{ getAptList.length }}개</span>
+				<span v-if="!isSelected">검색된 아파트 {{ getAptList.length }}개</span>
+				<button v-else @click="backToItemList" class="header__back-btn">
+					<AIcon type="arrow-left" />
+				</button>
 			</div>
 			<div class="side-bar__content">
-				<ul class="content__list" v-if="getAptList.length !== 0">
-					<li class="list__item" v-for="apt in getAptList" :key="apt.aptCode">
-						<div class="item__info">
-							<img
-								src="@/assets/sample-apt.jpg"
-								alt="sample"
-								class="info__image"
-							/>
-							<div class="info__desc">
-								<p class="desc__price">
-									매매 {{ apt.recentPrice | convertAptPrice }}
-								</p>
-								<h1 class="desc__title">아파트 · {{ apt.aptName }}</h1>
-								<p class="desc__sub-desc">건축년도 : {{ apt.buildYear }}년</p>
-								<p class="desc__sub-desc">
-									{{ apt.gugunName }} {{ apt.dongName }}
-								</p>
-								<div class="desc__badges">
-									<p class="badges__confirm-status">확인매물 21.11.09.</p>
-									<p
-										class="badges__lowest-price"
-										v-if="
-											getLowestPrice !== getHighestPrice &&
-											apt.recentPrice === getLowestPrice
-										"
-									>
-										최저가 검색 매물
+				<div v-if="!isSelected">
+					<ul class="content__list" v-if="getAptList.length !== 0">
+						<li
+							class="list__item"
+							v-for="apt in getAptList"
+							:key="apt.aptCode"
+							@click="selectItem(apt)"
+						>
+							<div class="item__info">
+								<img
+									src="@/assets/sample-apt.jpg"
+									alt="sample"
+									class="info__image"
+								/>
+								<div class="info__desc">
+									<p class="desc__price">
+										매매 {{ apt.recentPrice | convertAptPrice }}
 									</p>
-									<p
-										class="badges__highest-price"
-										v-if="
-											getLowestPrice !== getHighestPrice &&
-											apt.recentPrice === getHighestPrice
-										"
-									>
-										최고가 검색 매물
+									<h1 class="desc__title">아파트 · {{ apt.aptName }}</h1>
+									<p class="desc__sub-desc">건축연도: {{ apt.buildYear }}년</p>
+									<p class="desc__sub-desc">
+										{{ apt.gugunName }} {{ apt.dongName }}
 									</p>
+									<div class="desc__badges">
+										<p class="badges__confirm-status">확인매물 21.11.09.</p>
+										<p
+											class="badges__lowest-price"
+											v-if="
+												getLowestPrice !== getHighestPrice &&
+												apt.recentPrice === getLowestPrice
+											"
+										>
+											최저가 검색 매물
+										</p>
+										<p
+											class="badges__highest-price"
+											v-if="
+												getLowestPrice !== getHighestPrice &&
+												apt.recentPrice === getHighestPrice
+											"
+										>
+											최고가 검색 매물
+										</p>
+									</div>
 								</div>
 							</div>
-						</div>
-					</li>
-				</ul>
+						</li>
+					</ul>
+				</div>
+				<div class="content__detail" v-else>
+					<div class="detail__image">
+						<img src="@/assets/sample-apt.jpg" alt="" />
+					</div>
+					<div class="detail__header">
+						<h1 class="header__apt-name">{{ selectedItem.aptName }}</h1>
+						<p class="header__buildYear">
+							건축연도: {{ selectedItem.buildYear }}년
+						</p>
+					</div>
+					<div class="detail__main">
+						<p class="main__price">
+							매매 {{ selectedItem.recentPrice | convertAptPrice }}
+						</p>
+						<p class="main__address">
+							{{ selectedItem.sidoName }} {{ selectedItem.gugunName }}
+							{{ selectedItem.dongName }} {{ selectedItem.jibun }}
+						</p>
+					</div>
+					<div class="detail__ad">
+						<Advertisement />
+					</div>
+				</div>
 			</div>
 		</div>
 		<div class="main__map">
@@ -56,12 +89,20 @@
 </template>
 
 <script>
-import KakaoMap from '@/components/search/KakaoMap.vue';
 import { mapGetters } from 'vuex';
+import KakaoMap from '@/components/search/KakaoMap.vue';
+import Advertisement from '@/components/search/Advertisement.vue';
 
 export default {
 	components: {
 		KakaoMap,
+		Advertisement,
+	},
+	data() {
+		return {
+			selectedItem: {},
+			isSelected: false,
+		};
 	},
 	computed: {
 		...mapGetters('searchStore', [
@@ -82,9 +123,19 @@ export default {
 			return convertedPrice;
 		},
 	},
+	methods: {
+		selectItem(itemObject) {
+			this.isSelected = true;
+			this.selectedItem = itemObject;
+		},
+		backToItemList() {
+			this.isSelected = false;
+			this.selectedItem = {};
+		},
+	},
 };
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
 @import './scss/mapMain.scss';
 </style>

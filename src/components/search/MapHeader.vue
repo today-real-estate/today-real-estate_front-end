971c11217beed505f1c42d32a9be763355ab0a44
JeongHwan-dev <jeonghwan.dev@gmail.com>
2021-11-21 18:34:39 +0900

    Feat : 검색 '시, 구, 동' 필터 구현

--- src/components/search/MapHeader.vue
+++ src/components/search/MapHeader.vue
@@ -7,9 +7,9 @@
 		</div>
 		<div class="map__filters">
 			<div class="filters__select">
-				<!-- <select name="" id=""></select>
-				<select name="" id=""></select> -->
 				<ASelect
+					v-model="selectedSidoCode"
+					@change="selectSido"
 					style="
 						position: relative;
 						z-index: 30;
@@ -18,11 +18,13 @@
 						margin-right: 8px;
 					"
 				>
-					<!-- <a-select-option v-for="province in provinceData" :key="province">
-						{{ province }}
-					</a-select-option> -->
+					<ASelectOption v-for="sido in sidoList" :key="sido.code">
+						{{ sido.sidoName }}
+					</ASelectOption>
 				</ASelect>
 				<ASelect
+					v-model="selectedGugunCode"
+					@change="selectGugun"
 					style="
 						position: relative;
 						z-index: 30;
@@ -31,9 +33,24 @@
 						margin-right: 8px;
 					"
 				>
-					<!-- <a-select-option v-for="city in cities" :key="city">
-						{{ city }}
-					</a-select-option> -->
+					<ASelectOption v-for="gugun in gugunList" :key="gugun.code">
+						{{ gugun.gugunName }}
+					</ASelectOption>
+				</ASelect>
+				<ASelect
+					v-model="selectedDongCode"
+					@change="selectDong"
+					style="
+						position: relative;
+						z-index: 30;
+						width: 220px;
+						height: 34px;
+						margin-right: 8px;
+					"
+				>
+					<ASelectOption v-for="dong in dongList" :key="dong.code">
+						{{ dong.dongName }}
+					</ASelectOption>
 				</ASelect>
 			</div>
 			<div class="filters__btns">
@@ -49,7 +66,99 @@
 </template>
 
 <script>
-export default {};
+import { mapState, mapMutations, mapActions } from 'vuex';
+
+export default {
+	data() {
+		return {
+			selectedSidoCode: '선택하세요',
+			selectedGugunCode: '선택하세요',
+			selectedDongCode: '선택하세요',
+		};
+	},
+	computed: {
+		...mapState('searchStore', ['sidoList', 'gugunList', 'dongList']),
+	},
+	methods: {
+		...mapActions('searchStore', [
+			'GET_SIDO_LIST',
+			'GET_GUGUN_LIST',
+			'GET_DONG_LIST',
+			'GET_APT_LIST',
+		]),
+		...mapMutations('searchStore', [
+			'CLEAR_SIDO_LIST',
+			'CLEAR_GUGUN_LIST',
+			'CLEAR_DONG_LIST',
+		]),
+		async getSidoList() {
+			try {
+				await this.GET_SIDO_LIST();
+			} catch (error) {
+				console.log(error);
+			}
+		},
+		async getGugunList(sidoCode) {
+			const sidoData = {
+				sido: sidoCode,
+			};
+
+			try {
+				await this.GET_GUGUN_LIST(sidoData);
+			} catch (error) {
+				console.log(error);
+			}
+		},
+		async getDongList(gugunCode) {
+			const gugunData = {
+				gugun: gugunCode,
+			};
+
+			try {
+				await this.GET_DONG_LIST(gugunData);
+			} catch (error) {
+				console.log(error);
+			}
+		},
+		async getAptList(dongCode) {
+			const dongData = {
+				dong: dongCode,
+			};
+
+			try {
+				await this.GET_APT_LIST(dongData);
+			} catch (error) {
+				console.log(error);
+			}
+		},
+		selectSido() {
+			this.CLEAR_GUGUN_LIST();
+			this.CLEAR_DONG_LIST();
+			this.selectedGugunCode = '선택하세요';
+			this.selectedDongCode = '선택하세요';
+
+			if (!isNaN(this.selectedSidoCode)) {
+				this.getGugunList(this.selectedSidoCode);
+			}
+		},
+		selectGugun() {
+			this.CLEAR_DONG_LIST();
+			this.selectedDongCode = '선택하세요';
+
+			if (!isNaN(this.selectedGugunCode)) {
+				this.getDongList(this.selectedGugunCode);
+			}
+		},
+		selectDong() {
+			if (!isNaN(this.selectedDongCode)) {
+				this.getAptList(this.selectedDongCode);
+			}
+		},
+	},
+	created() {
+		this.getSidoList();
+	},
+};
 </script>
 
 <style lang="scss" scoped>
--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,14 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
 import userStore from '@/store/modules/userStore';
+import searchStore from '@/store/modules/searchStore';
 
 Vue.use(Vuex);
 
 const store = new Vuex.Store({
 	modules: {
 		userStore,
+		searchStore,
 	},
 });
 

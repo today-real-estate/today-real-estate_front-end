e6d86a7368260f845601cf5b40afb4337b6a82c5
JeongHwan-dev <jeonghwan.dev@gmail.com>
2021-11-23 18:28:41 +0900

    배너 광고 컴포넌트 및 이벤트 컴포넌트 구현

--- src/views/MainPage.vue
+++ src/views/MainPage.vue
@@ -2,17 +2,23 @@
 	<div>
 		<MainSearch />
 		<Recommendation />
+		<EventBanner />
+		<ApplicationAd />
 	</div>
 </template>
 
 <script>
 import MainSearch from '@/components/main/MainSearch.vue';
 import Recommendation from '@/components/main/Recommendation.vue';
+import EventBanner from '@/components/main/EventBanner.vue';
+import ApplicationAd from '@/components/main/ApplicationAd.vue';
 
 export default {
 	components: {
 		MainSearch,
 		Recommendation,
+		EventBanner,
+		ApplicationAd,
 	},
 };
 </script>

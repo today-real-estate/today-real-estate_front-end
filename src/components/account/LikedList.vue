1ece1edd8e5b6d04d98ce6c04ac2e688f161b7b5
JeongHwan-dev <jeonghwan.dev@gmail.com>
2021-11-25 15:17:03 +0900

    Feat : 오늘의 부동산 뉴스 페이지 구현

--- src/components/account/LikedList.vue
+++ src/components/account/LikedList.vue
@@ -1,6 +1,23 @@
 <template>
 	<div class="liked-list-container">
-		<div class="liked-list-container___inner"></div>
+		<div class="liked-list-container___inner">
+			<div class="list__apt">
+				<div class="apt__images">
+					<img src="@/assets/sample-apt.jpg" alt="sample" />
+				</div>
+				<div class="apt__apt-info">
+					<h2 class="apt-info__name">용산파크타워(24-0)</h2>
+					<h1 class="apt-info__price">
+						{{ apt.recentPrice | convertAptPrice }}
+					</h1>
+					<p class="apt-info__build-year">건축연도: {{ apt.buildYear }}</p>
+					<p class="apt-info__address">
+						{{ apt.sidoName }} {{ apt.gugunName }} {{ apt.dongName }}
+						{{ apt.jibun }}
+					</p>
+				</div>
+			</div>
+		</div>
 	</div>
 </template>
 
--- src/views/NewsPage.vue
+++ src/views/NewsPage.vue
@@ -5,192 +5,65 @@
 				<h1>오늘의 부동산 뉴스</h1>
 			</div>
 			<div class="news__main">
-				<ul class="contents__list">
-					<li class="list__item">
-						<div class="item__image">
-							<img src="@/images/apt-ad-banner.jpg" alt="" />
-						</div>
-						<div class="item__contents">
-							<a href="" class="contents__title">
-								5000억 송현동 부지와 바꿀 삼성동 서울의료원 땅 다음달 확정
-							</a>
-							<p class="contents__text">
-								[서울시, 삼성동 서울의료원 남측 부지 특별계획구역 변경] 약
-								5000억원 규모로 추정되는 종로구 송현동 대한항공 부지와 맞바꿀
-								강남구 삼성동 옛 서울의료원 남측 부지의 구체적인 면적이
-								...머니투데이
-							</p>
-							<span class="contents__date">2021.11.25</span>
-						</div>
-					</li>
-					<li class="list__item">
-						<div class="item__image">
-							<img src="@/images/apt-ad-banner.jpg" alt="" />
-						</div>
-						<div class="item__contents">
-							<a href="" class="contents__title">
-								5000억 송현동 부지와 바꿀 삼성동 서울의료원 땅 다음달 확정
-							</a>
-							<p class="contents__text">
-								[서울시, 삼성동 서울의료원 남측 부지 특별계획구역 변경] 약
-								5000억원 규모로 추정되는 종로구 송현동 대한항공 부지와 맞바꿀
-								강남구 삼성동 옛 서울의료원 남측 부지의 구체적인 면적이
-								...머니투데이
-							</p>
-							<span class="contents__date">2021.11.25</span>
-						</div>
-					</li>
-					<li class="list__item">
-						<div class="item__image">
-							<img src="@/images/apt-ad-banner.jpg" alt="" />
-						</div>
-						<div class="item__contents">
-							<a href="" class="contents__title">
-								5000억 송현동 부지와 바꿀 삼성동 서울의료원 땅 다음달 확정
-							</a>
-							<p class="contents__text">
-								[서울시, 삼성동 서울의료원 남측 부지 특별계획구역 변경] 약
-								5000억원 규모로 추정되는 종로구 송현동 대한항공 부지와 맞바꿀
-								강남구 삼성동 옛 서울의료원 남측 부지의 구체적인 면적이
-								...머니투데이
-							</p>
-							<span class="contents__date">2021.11.25</span>
-						</div>
-					</li>
-					<li class="list__item">
-						<div class="item__image">
-							<img src="@/images/apt-ad-banner.jpg" alt="" />
-						</div>
-						<div class="item__contents">
-							<a href="" class="contents__title">
-								5000억 송현동 부지와 바꿀 삼성동 서울의료원 땅 다음달 확정
-							</a>
-							<p class="contents__text">
-								[서울시, 삼성동 서울의료원 남측 부지 특별계획구역 변경] 약
-								5000억원 규모로 추정되는 종로구 송현동 대한항공 부지와 맞바꿀
-								강남구 삼성동 옛 서울의료원 남측 부지의 구체적인 면적이
-								...머니투데이
-							</p>
-							<span class="contents__date">2021.11.25</span>
-						</div>
-					</li>
-					<li class="list__item">
-						<div class="item__image">
-							<img src="@/images/apt-ad-banner.jpg" alt="" />
-						</div>
-						<div class="item__contents">
-							<a href="" class="contents__title">
-								5000억 송현동 부지와 바꿀 삼성동 서울의료원 땅 다음달 확정
-							</a>
-							<p class="contents__text">
-								[서울시, 삼성동 서울의료원 남측 부지 특별계획구역 변경] 약
-								5000억원 규모로 추정되는 종로구 송현동 대한항공 부지와 맞바꿀
-								강남구 삼성동 옛 서울의료원 남측 부지의 구체적인 면적이
-								...머니투데이
-							</p>
-							<span class="contents__date">2021.11.25</span>
-						</div>
-					</li>
-					<li class="list__item">
-						<div class="item__image">
-							<img src="@/images/apt-ad-banner.jpg" alt="" />
-						</div>
-						<div class="item__contents">
-							<a href="" class="contents__title">
-								5000억 송현동 부지와 바꿀 삼성동 서울의료원 땅 다음달 확정
-							</a>
-							<p class="contents__text">
-								[서울시, 삼성동 서울의료원 남측 부지 특별계획구역 변경] 약
-								5000억원 규모로 추정되는 종로구 송현동 대한항공 부지와 맞바꿀
-								강남구 삼성동 옛 서울의료원 남측 부지의 구체적인 면적이
-								...머니투데이
-							</p>
-							<span class="contents__date">2021.11.25</span>
-						</div>
-					</li>
+				<ul class="main__list">
+					<NewsItem
+						v-for="(newsItem, index) in newsList[current]"
+						:key="index"
+						:news="newsItem"
+					/>
 				</ul>
 			</div>
 			<div class="news__pagination">
-				<APagination v-model="current" :total="50" show-less-items />
+				<APagination v-model="current" :total="30" show-less-items />
 			</div>
 		</div>
 	</div>
 </template>
 
 <script>
+import { fetchNewsList } from '@/api/news';
+import NewsItem from '@/components/news/NewsItem.vue';
+
 export default {
+	components: {
+		NewsItem,
+	},
 	data() {
 		return {
+			currentPageNewsList: [],
+			newsList: [],
 			current: 1,
 		};
+	},
+	created() {
+		this.initNewsList();
+	},
+	methods: {
+		division(array, n) {
+			const len = array.length;
+			const count = Math.floor(len / n) + (Math.floor(len % n) > 0 ? 1 : 0);
+			const temp = [];
+
+			for (let i = 0; i < count; i++) {
+				temp.push(array.splice(0, n));
+			}
+
+			return temp;
+		},
+		async initNewsList() {
+			try {
+				const { data } = await fetchNewsList();
+				const filteredNewsList = data.filter((news) => news.imageUrl);
+
+				this.newsList = this.division(filteredNewsList, 6);
+			} catch (error) {
+				console.log(error);
+			}
+		},
 	},
 };
 </script>
 
 <style lang="scss" scoped>
-.news-container {
-	background-color: #f9f9f9;
-	padding: 100px 0;
-
-	.news-container__inner {
-		max-width: 1100px;
-		min-width: 900px;
-		margin: 0 auto;
-		padding: 0 10px;
-
-		.news__header {
-			margin-bottom: 50px;
-
-			h1 {
-				margin-bottom: 0;
-				padding: 10px;
-				border-bottom: 3px solid #e8e8e8;
-				border-radius: 3px;
-				font-size: 36px;
-				font-weight: 500;
-			}
-		}
-
-		.news__main {
-			.contents__list {
-				padding: 0 12px;
-
-				.list__item {
-					display: flex;
-					height: 130px;
-					margin-bottom: 14px;
-
-					.item__image {
-						margin-right: 18px;
-
-						img {
-							width: 240px;
-							border-radius: 3px;
-						}
-					}
-					.item__contents {
-						.contents__title {
-							font-size: 18px;
-							font-weight: 500;
-							color: #019cdc;
-						}
-						.contents__text {
-							font-weight: 300;
-							margin-bottom: 8px;
-						}
-						.contents__date {
-							font-size: 13px;
-							font-weight: 300;
-						}
-					}
-				}
-			}
-		}
-
-		.news__pagination {
-			margin-top: 80px;
-			text-align: center;
-		}
-	}
-}
+@import './scss/newsPage.scss';
 </style>
